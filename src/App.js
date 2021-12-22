417981e2767955ba4382a56928e534c4eaeee61d
unknown <tamiloreolaniyi@gmail.com>
2021-12-23 00:43:10 +0100

    Header Component and Routing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/Shop/shop.component';
+import Header from './components/header/Header.jsx'
 
 
 function App() {
   return (
     <div >
+      <Header/>
       <Switch>
       <Route exact path='/' component={HomePage}></Route>
       <Route path='/shop' component={ShopPage}></Route>
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import { ReactComponent as Logo } from '../../assets/4.3 crown.svg'
+
+import './Header.scss'
+
+const Header = () => (
+    <div className='header'>
+        <Link to="/" className='logo-container'>
+            <Logo className='logo'/>
+        </Link>
+        <div className="options">
+            <Link to='/shop' className='option'>
+                SHOP
+            </Link>
+            <Link to='/shop' className='option'>
+                CONTACT
+            </Link>
+            <Link to='/shop' className='option'></Link>
+        </div>
+    </div>
+)
+
+export default Header
